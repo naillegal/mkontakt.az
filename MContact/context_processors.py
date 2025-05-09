6cb0bef5799bd2f6d3bd573625bd33e0badfed65
naillegal <quluzade.nail@gmail.com>
2025-05-09 17:07:28 +0400

    context_processors.py issues resolved

--- MContact/context_processors.py
+++ MContact/context_processors.py
@@ -3,19 +3,23 @@ from .models import ContactInfo, Brand, User
 
 def contact_info_processor(request):
     return {
-        'contact_info': ContactInfo.objects.first(),
-        'footer_brands': Brand.objects.all().order_by('-created_at')[:5],
+        "contact_info": ContactInfo.objects.first(),
+        "footer_brands": Brand.objects.order_by("-created_at")[:5],
     }
 
+
 def user_context(request):
+    if request.path.startswith("/admin"):
+        return {}
+
     uid = request.session.get("user_id")
-    print("user_id from session:", uid)
+    custom_user = None
     if uid:
         try:
-            user = User.objects.get(pk=uid)
-            print("user from DB:", user)
-            return {"user": user}
+            custom_user = User.objects.get(pk=uid)
         except User.DoesNotExist:
             pass
-    return {"user": None}
 
+    return {
+        "user": custom_user
+    }
