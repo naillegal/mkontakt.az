5109034f5bbb6f71d19a89b12ffc6e412172594d
naillegal <quluzade.nail@gmail.com>
2025-08-02 13:54:57 +0400

    notification issues resolved

--- MContact/firebase_config.py
+++ MContact/firebase_config.py
@@ -1,7 +1,12 @@
+import os
+from django.conf import settings
 import firebase_admin
 from firebase_admin import credentials
+
+cred_path = os.path.join(settings.BASE_DIR, 'mkontakt-firebase.json')
+cred = credentials.Certificate(cred_path)
 
 try:
     firebase_admin.get_app()
 except ValueError:
-    firebase_admin.initialize_app()
+    firebase_admin.initialize_app(cred)
